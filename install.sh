1cbdbf106e1ccfbb3ab65e2b08bd18c9b88a8694
swamshraddha <80090983+swamshraddha@users.noreply.github.com>
2022-11-05 01:20:03 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt -y install docker-ce
+sudo systemctl status docker
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+su - $(whoami)
